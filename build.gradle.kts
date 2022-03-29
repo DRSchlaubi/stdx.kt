10e1d168e0d1bf9db3842730f2eb79dce4c4f390
Michael Rittmeister <michael@rittmeister.in>
2022-03-29 20:20:43 +0200

    [ci skip] ci: Remove automatic maven central deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.jetbrains.dokka")
-    id("io.codearte.nexus-staging") version "0.30.0"
 }
 
 
@@ -16,11 +15,4 @@ tasks {
     dokkaHtmlMultiModule {
         outputDirectory.set(rootProject.file("docs"))
     }
-}
-
-nexusStaging {
-    serverUrl = "https://s01.oss.sonatype.org/service/local/"
-    packageGroup = "dev.schlaubi"
-    username = System.getenv("SONATYPE_USER")
-    password = System.getenv("SONATYPE_KEY")
 }
