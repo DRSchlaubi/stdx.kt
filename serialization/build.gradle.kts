237a706a293db4a7d98e882c18eb9b03937c7399
Michael Rittmeister <michael@rittmeister.in>
2022-04-06 19:28:56 +0200

    Fix kbson test dependency

--- serialization/build.gradle.kts
+++ serialization/build.gradle.kts
@@ -2,12 +2,11 @@ plugins {
     `stdx-module`
     `stdx-ktlint`
     `stdx-publishing`
+    `all-platforms`
     kotlin("plugin.serialization")
 }
 
 kotlin {
-    all()
-
     sourceSets {
         commonMain {
             dependencies {
@@ -19,6 +18,11 @@ kotlin {
             dependencies {
                 implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.3.2")
                 implementation("org.jetbrains.kotlinx", "kotlinx-serialization-protobuf", "1.3.2")
+            }
+        }
+
+        jvmTest {
+            dependencies {
                 implementation("com.github.jershell", "kbson", "0.4.4")
             }
         }
