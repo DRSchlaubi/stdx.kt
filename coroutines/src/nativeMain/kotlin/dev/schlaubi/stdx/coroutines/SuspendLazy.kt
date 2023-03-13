f9096cd48423a5d2d2cc633414869100335cd7ca
Michael Rittmeister <michael@rittmeister.in>
2023-03-13 16:10:52 +0100

    Update native suspendLazy implementation

--- coroutines/src/nativeMain/kotlin/dev/schlaubi/stdx/coroutines/SuspendLazy.kt
+++ coroutines/src/nativeMain/kotlin/dev/schlaubi/stdx/coroutines/SuspendLazy.kt
@@ -2,10 +2,7 @@ package dev.schlaubi.stdx.coroutines
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlin.native.concurrent.FreezableAtomicReference
-import kotlin.native.concurrent.InvalidMutabilityException
-import kotlin.native.concurrent.freeze
-import kotlin.native.concurrent.isFrozen
+import kotlin.native.concurrent.*
 
 private fun throwMMError(): Nothing =
     throw UnsupportedOperationException("suspendLazy only supports the new memory model")
@@ -36,15 +33,15 @@ public actual fun <T> suspendLazy(initializer: SuspendingInitializer<T>): Suspen
 public actual fun <T> suspendLazy(mode: LazyThreadSafetyMode, initializer: SuspendingInitializer<T>): SuspendLazy<T> =
     when (mode) {
         LazyThreadSafetyMode.SYNCHRONIZED -> if (isExperimentalMM()) SynchronizedLazyImpl(initializer) else throwMMError()
-        LazyThreadSafetyMode.PUBLICATION -> if (isExperimentalMM()) SynchronizedLazyImpl(initializer) else throwMMError()
+        LazyThreadSafetyMode.PUBLICATION -> if (isExperimentalMM()) SafePublicationLazyImpl(initializer) else throwMMError()
         LazyThreadSafetyMode.NONE -> UnsafeSuspendLazyImpl(initializer)
     }
 
 
 @Suppress("UNCHECKED_CAST")
 internal class SynchronizedLazyImpl<out T>(initializer: SuspendingInitializer<T>) : SuspendLazy<T> {
-    private var initializer = FreezableAtomicReference<SuspendingInitializer<T>?>(initializer)
-    private var valueRef = FreezableAtomicReference<Any?>(UNINITIALIZED_VALUE)
+    private var initializer = AtomicReference<SuspendingInitializer<T>?>(initializer)
+    private var valueRef = AtomicReference<Any?>(UNINITIALIZED_VALUE)
     private val lock = Mutex()
 
     @Suppress("LocalVariableName")
@@ -57,14 +54,7 @@ internal class SynchronizedLazyImpl<out T>(initializer: SuspendingInitializer<T>
         return lock.withLock {
             val _v2 = valueRef.value
             if (_v2 === UNINITIALIZED_VALUE) {
-                val wasFrozen = this.isFrozen
                 val typedValue = initializer.value!!()
-                if (this.isFrozen) {
-                    if (!wasFrozen) {
-                        throw InvalidMutabilityException("Frozen during lazy computation")
-                    }
-                    typedValue.freeze()
-                }
                 valueRef.value = typedValue
                 initializer.value = null
                 typedValue
@@ -83,8 +73,8 @@ internal class SynchronizedLazyImpl<out T>(initializer: SuspendingInitializer<T>
 
 @Suppress("UNCHECKED_CAST")
 internal class SafePublicationLazyImpl<out T>(initializer: SuspendingInitializer<T>) : SuspendLazy<T> {
-    private var initializer = FreezableAtomicReference<SuspendingInitializer<T>?>(initializer)
-    private var valueRef = FreezableAtomicReference<Any?>(UNINITIALIZED_VALUE)
+    private var initializer = AtomicReference<SuspendingInitializer<T>?>(initializer)
+    private var valueRef = AtomicReference<Any?>(UNINITIALIZED_VALUE)
 
     override suspend fun get(): T {
         val value = valueRef.value
@@ -95,14 +85,7 @@ internal class SafePublicationLazyImpl<out T>(initializer: SuspendingInitializer
         val initializerValue = initializer.value
         // if we see null in initializer here, it means that the value is already set by another thread
         if (initializerValue != null) {
-            val wasFrozen = this.isFrozen
             val newValue = initializerValue()
-            if (this.isFrozen) {
-                if (!wasFrozen) {
-                    throw InvalidMutabilityException("Frozen during lazy computation")
-                }
-                newValue.freeze()
-            }
             if (valueRef.compareAndSet(UNINITIALIZED_VALUE, newValue)) {
                 initializer.value = null
                 return newValue
